994355f66f9fe8d4ef8acc68c4443bf89f8b374a
McDermott, Thomas E <thomas.mcdermott@pnnl.gov>
2023-06-26 13:57:38 -0400

    factored the profile functions into mpow_utilities

---
+++ bes/most.py
@@ -0,0 +1,115 @@
+import sys
+import os
+import math
+import subprocess
+import numpy as np
+from tests import mpow_utilities as mpow
+
+# some data and utilities from CIMHub/BES/mpow.py
+
+CASES = [
+  {'id': '1783D2A8-1204-4781-A0B4-7A73A2FA6038', 
+   'name': 'IEEE118', 
+   'swingbus':'131',
+   'load_scale':0.6748},
+  {'id': '2540AF5C-4F83-4C0F-9577-DEE8CC73BBB3', 
+   'name': 'WECC240', 
+   'swingbus':'2438',
+   'load_scale':1.0425},
+]
+
+FUELS = {
+  'hydro':  {'c2':1.0e-5, 'c1': 1.29, 'c0': 0.0},
+  'wind':   {'c2':1.0e-5, 'c1': 0.01, 'c0': 0.0},
+  'solar':  {'c2':1.0e-5, 'c1': 0.01, 'c0': 0.0},
+  'coal':   {'c2':0.0009, 'c1': 19.0, 'c0': 2128.0},
+  'ng':     {'c2':0.0060, 'c1': 45.0, 'c0': 2230.0},
+  'nuclear':{'c2':0.00019, 'c1': 8.0, 'c0': 1250.0}
+}
+
+# global constants
+SQRT3 = math.sqrt(3.0)
+RAD_TO_DEG = 180.0 / math.pi
+MVA_BASE = 100.0
+
+def get_gencosts(fuel):
+  c2 = 0.0
+  c1 = 0.0
+  c0 = 0.0
+  if fuel in FUELS:
+    c2 = FUELS[fuel]['c2']
+    c1 = FUELS[fuel]['c1']
+    c0 = FUELS[fuel]['c0']
+  return c2, c1, c0
+
+# sample code from TESP that automates Matpower in Octave
+# https://github.com/pnnl/tesp/blob/develop/examples/capabilities/ercot/case8/tso_most.py
+
+if sys.platform == 'win32':
+  octave = '"C:\Program Files\GNU Octave\Octave-8.2.0\octave-launch.exe" --no-gui'
+else:
+  octave = 'octave'
+
+def write_solve_file (root, load_scale):
+  fscript = 'solve{:s}.m'.format(root)
+  fsolved = '{:s}solved.m'.format(root)
+  fsummary = '{:s}summary.txt'.format(root)
+# fbus = '{:s}bus.txt'.format(root.lower())
+# fgen = '{:s}gen.txt'.format(root.lower())
+# fbranch = '{:s}branch.txt'.format(root.lower())
+  fp = open (fscript, 'w')
+  print ("""clear;""", file=fp)
+  print ("""cd {:s}""".format (os.getcwd()), file=fp)
+  print ("""mpc = loadcase({:s});""".format (root.upper()), file=fp)
+# print ("""case_info(mpc);""", file=fp)
+  print ("""mpc = scale_load({:.5f},mpc);""".format (load_scale), file=fp)
+  print ("""opt1 = mpoption('out.all', 0, 'verbose', 0);""", file=fp)
+  print ("""results=runpf(mpc, opt1);""", file=fp)
+  print ("""define_constants;""", file=fp)
+# print ("""codes=matpower_gen_type(results.gentype);""", file=fp)
+# print ("""mgen=[results.gen(:,GEN_BUS),results.gen(:,PG),results.gen(:,QG),codes];""", file=fp)
+# print ("""mbus=[results.bus(:,VM),results.bus(:,VA),results.bus(:,PD),results.bus(:,QD)];""", file=fp)
+# print ("""mbranch=[results.branch(:,F_BUS),results.branch(:,T_BUS),results.branch(:,TAP),results.branch(:,SHIFT),results.branch(:,PF),results.branch(:,QF),results.branch(:,PT),results.branch(:,QT)];""", file=fp)
+# print ("""csvwrite('{:s}',mgen);""".format (fgen), file=fp)
+# print ("""csvwrite('{:s}',mbus);""".format (fbus), file=fp)
+# print ("""csvwrite('{:s}',mbranch);""".format (fbranch), file=fp)
+  print ("""opt2 = mpoption('out.sys_sum', 1, 'out.bus', 0, 'out.branch', 0);""", file=fp)
+  print ("""fd = fopen('{:s}', 'w');""".format (fsummary), file=fp)
+  print ("""fprintf(fd,'results.success = %d\\n', results.success);""", file=fp)
+  print ("""fprintf(fd,'results.iterations = %d\\n', results.iterations);""", file=fp)
+  print ("""fprintf(fd,'results.et = %.4f\\n', results.et);""", file=fp)
+  print ("""printpf(results, fd, opt2);""", file=fp)
+  print ("""fclose(fd);""", file=fp)
+  print ("""savecase('{:s}', results);""".format (fsolved), file=fp)
+  print ("""exit;""", file=fp)
+  fp.close()
+  return fscript, fsolved, fsummary
+
+if __name__ == '__main__':
+  case_id = 0
+  if len(sys.argv) > 1:
+    case_id = int(sys.argv[1])
+  sys_name = CASES[case_id]['name']
+  load_scale = CASES[case_id]['load_scale']
+  d = mpow.read_matpower_casefile ('{:s}.m'.format (sys_name))
+  mpow.summarize_casefile (d, 'Input')
+  fscript, fsolved, fsummary = write_solve_file (sys_name, load_scale)
+  cmdline = '{:s} {:s}'.format(octave, fscript)
+  print ('running', cmdline)
+  proc = subprocess.Popen(cmdline, shell=True)
+  proc.wait()
+  mpow.print_solution_summary (fsummary, details=True)
+  r = mpow.read_matpower_casefile (fsolved)
+  for tag in ['bus', 'gen', 'branch', 'gencost']:
+    r[tag] = np.array(r[tag], dtype=float)
+  mpow.summarize_casefile (r, 'Solved')
+  print ('Min and max bus voltages=[{:.4f},{:.4f}]'.format (np.min(r['bus'][:,mpow.VM]),np.max(r['bus'][:,mpow.VM])))
+  print ('Load = {:.3f} + j{:.3f} MVA'.format (np.sum(r['bus'][:,mpow.PD]),np.sum(r['bus'][:,mpow.QD])))
+  print ('Gen =  {:.3f} + j{:.3f} MVA'.format (np.sum(r['gen'][:,mpow.PG]),np.sum(r['gen'][:,mpow.QG])))
+  gen_online = np.array(r['gen'][:,mpow.GEN_STATUS]>0)
+  print ('{:d} of {:d} generators on line'.format (int(np.sum(gen_online)),r['gen'].shape[0]))
+  pgmax = np.sum(r['gen'][:,mpow.PMAX], where=gen_online)
+  qgmax = np.sum(r['gen'][:,mpow.QMAX], where=gen_online)
+  qgmin = np.sum(r['gen'][:,mpow.QMIN], where=gen_online)
+  print ('Online capacity = {:.2f} MW, {:.2f} to {:.2f} MVAR'.format (pgmax, qgmin, qgmax))
+
--- bes/tests/mpow_utilities.py
+++ bes/tests/mpow_utilities.py
@@ -107,6 +107,21 @@ DCL_MU_QMAXF=21-1
 DCL_MU_QMINT=22-1
 DCL_MU_QMAXT=23-1
 
+ercot8_wind_plants = np.array ([99.8, 1657.0, 2242.2, 3562.2, 8730.3])
+ercot8_wind_plant_rows = [14, 15, 16, 17, 18]
+ercot8_wind_plant_buses = [1, 3, 4, 6, 7]
+ercot8_load_rows = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# archived load data from TESP example of ERCOT8
+ercot8_base_load = np.array ([[7182.65, 6831.0, 6728.83, 6781.1, 6985.44, 7291.94, 7650.72, 8104.54, 8522.71, 8874.36, 9173.74, 9446.98, 9736.85, 10078.99, 10466.28, 10855.94, 11179.08, 11319.26, 11200.46, 10893.96, 10630.22, 10326.1, 9781.99, 8810.21],
+[7726.69, 6840.09, 6637.09, 6603.94, 6719.95, 6972.67, 7295.82, 7693.55, 8140.99, 8518.01, 8837.02, 9114.6, 9383.9, 9686.33, 10046.77, 10436.22, 10800.8, 11053.52, 11078.38, 10862.95, 10560.51, 10311.93, 9930.77, 9255.46],
+[162.23, 150.74, 147.38, 147.58, 151.14, 157.34, 164.74, 174.24, 183.88, 191.86, 198.73, 204.8, 210.94, 218.0, 226.31, 234.89, 242.62, 247.1, 246.18, 240.24, 233.97, 228.03, 218.06, 200.84],
+[2097.83, 1715.67, 1634.79, 1612.55, 1625.69, 1676.24, 1750.04, 1836.99, 1946.17, 2045.25, 2129.17, 2200.95, 2265.65, 2335.41, 2418.31, 2511.32, 2604.34, 2680.16, 2712.51, 2681.17, 2608.38, 2544.69, 2470.88, 2337.43],
+[3922.54, 3492.58, 3390.92, 3376.09, 3437.51, 3568.83, 3731.92, 3937.36, 4163.99, 4356.73, 4519.81, 4661.72, 4801.51, 4956.12, 5140.39, 5337.36, 5523.74, 5650.82, 5663.53, 5549.16, 5396.66, 5267.47, 5070.49, 4723.14],
+[232.03, 198.01, 191.14, 189.66, 192.44, 199.23, 208.37, 219.33, 232.2, 243.34, 252.82, 260.91, 268.66, 277.18, 287.27, 298.32, 309.02, 316.94, 318.77, 313.37, 304.59, 297.45, 287.36, 269.27],
+[2650.16, 2535.62, 2503.87, 2529.95, 2610.47, 2727.27, 2864.48, 3034.58, 3187.67, 3315.82, 3425.81, 3527.87, 3636.74, 3766.01, 3911.17, 4055.18, 4170.85, 4212.81, 4159.51, 4043.84, 3947.45, 3827.25, 3612.92, 3213.76],
+[56.55, 51.95, 50.65, 50.57, 51.68, 53.72, 56.25, 59.4, 62.76, 65.57, 67.97, 70.06, 72.15, 74.53, 77.34, 80.29, 83.0, 84.73, 84.61, 82.72, 80.5, 78.53, 75.32, 69.74]])
+
 
 ###################################################
 # from tesp_support package, parse_msout.py
@@ -282,4 +297,70 @@ def write_most_table_indices(fp):
     MU_PMAX, MU_PMIN, MU_QMAX, MU_QMIN, PC1, PC2, QC1MIN, QC1MAX, ...
     QC2MIN, QC2MAX, RAMP_AGC, RAMP_10, RAMP_30, RAMP_Q, APF] = idx_gen;
   [PW_LINEAR, POLYNOMIAL, MODEL, STARTUP, SHUTDOWN, NCOST, COST] = idx_cost;""", file=fp)
+
+def write_unresponsive_load_profile (root, rows, data, scale):
+  fp = open('{:s}.m'.format(root), 'w')
+  print('function unresp = {:s}'.format(root), file=fp)
+  write_most_table_indices(fp)
+  print("""  unresp = struct( ...
+    'type', 'mpcData', ...
+    'table', CT_TBUS, ...
+    'rows', {:s}, ...
+    'col', PD, ...
+    'chgtype', CT_REP, ...
+    'values', [] );""".format(str(rows)), file=fp)
+  print('  scale = {:.3f};'.format(scale), file=fp)
+  for i in range(len(rows)):
+    rownum = rows[i]
+    vals = str([round(v, 2) for v in data[i,:]])
+    mvals = vals.replace(',', ';')
+    print("""  unresp.values(:, 1, {:d}) = scale * {:s};""".format(rownum, mvals), file=fp)
+  print('end', file=fp)
+  fp.close()
+
+def write_responsive_load_profile (root, rows, data, scale, fixed_root):
+  fp = open('{:s}.m'.format(root), 'w')
+  print('function resp = {:s}'.format(root), file=fp)
+  write_most_table_indices(fp)
+  print("""  resp = struct( ...
+    'type', 'mpcData', ...
+    'table', CT_TLOAD, ...
+    'rows', {:s}, ...
+    'col', CT_LOAD_DIS_P, ...
+    'chgtype', CT_REP, ...
+    'values', [] );""".format(str(rows)), file=fp)
+  print('  scale = {:.3f};'.format(scale), file=fp)
+  for i in range(len(rows)):
+    rownum = rows[i]
+    vals = str([round(v, 2) for v in data[i,:]])
+    mvals = vals.replace(',', ';')
+    print("""  resp.values(:, 1, {:d}) = scale * {:s};""".format(rownum, mvals), file=fp)
+  print("""
+    # per answer to https://github.com/MATPOWER/matpower/issues/106
+    #   apply scaling to the total of responsive plus unresponsive load
+    unresp = {:s};
+    resp.values = resp.values + unresp.values;""".format(fixed_root), file=fp)
+  print('end', file=fp)
+  fp.close()
+
+def write_wind_profile (root, rows, data):
+  fp = open('{:s}.m'.format(root), 'w')
+  print('function wind = {:s}'.format(root), file=fp)
+  write_most_table_indices(fp)
+  print("""  wind = struct( ...
+    'type', 'mpcData', ...
+    'table', CT_TGEN, ...
+    'rows', {:s}, ...
+    'col', PMAX, ...
+    'chgtype', CT_REP, ...
+    'values', [] );""".format(str(rows)), file=fp)
+  for i in range(len(rows)):
+    vals = str([round(v, 2) for v in data[i,:]])
+    mvals = vals.replace(',', ';')
+    print("""  wind.values(:, 1, {:d}) = {:s};""".format(i+1, mvals), file=fp)
+  print('end', file=fp)
+  fp.close()
+
+
+
 
