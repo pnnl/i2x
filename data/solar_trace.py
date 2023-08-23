ccaacfaada47d47ac038cd2830f043f599a6f7d5
varoozhan <varoozhan@gmail.com>
2023-08-23 13:08:51 -0700

    Cleaned up the workflow yml file. Added queues_2021_clean_data.xlsx to the input/archive folder. Started comment clean up.

--- data/solar_trace.py
+++ data/solar_trace.py
@@ -15,24 +15,25 @@ PARENT_DIR = SCRIPT_DIR.parent # Get the parent directory
 OUTPUT_DIR = PARENT_DIR / 'hubdata' / 'output' / 'solarTRACE.csv'# Get the output directory
 
 def st_driver():
+    # 
     ahj_utility_df = read_input(SOLARTRACE, SOLARTRACE_SHEET, 2, 0)
-    # print("ahj_utility_df: ", ahj_utility_df)
+    # 
     if ahj_utility_df is None:
         print('File Not found in directory.')
     renamed_df = rename_cols(ahj_utility_df)
-    # print("renamed_df: ", renamed_df)
+    # 
     agg_df = aggregate(renamed_df)
-    # print('agg_df ', agg_df)
+    # 
     total_df = total(agg_df)
     #
     weighted_df = weighted_pre(total_df)
+    # 
     full_df = full(weighted_df, total_df)
+    # 
     full_df = add_state_full(full_df)
-    # print('full_df ', full_df)
-    # start = time.time()
+    # 
     api_df = api_call(full_df)
-    # end = time.time()
-    # print('API CALL TIME: ',end-start)
+    # 
     final_df = final_processing(api_df,weighted_df)
     
     #TESTING CODE
