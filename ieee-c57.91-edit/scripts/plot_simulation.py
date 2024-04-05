7142415acb38d551d4b2d0a8e0af0228225ebf25
Eran Schweitzer <eran.schweitzer@pnnl.gov>
2024-04-04 22:52:33 -0700

    setting up and running test matrix

--- ieee-c57.91-edit/scripts/plot_simulation.py
+++ ieee-c57.91-edit/scripts/plot_simulation.py
@@ -2,22 +2,34 @@
 import transformer_thermal_models as xmdl
 from transformer_thermal_models import Transformer, LoadConditions
 import pandas as pd
-import argparse, warnings, os
+import argparse, warnings, os, io, json
+import tempfile
 
-def main(xfrm_config, load_config, savename):
+def main(h5path, key, savename):
     
     print("loading data...", end="")
-    #Load transformer data
-    xfrm = Transformer()
-    xfrm.from_json(xfrm_config)
-    tmp = pd.read_csv(savename, parse_dates=[0])
-    tmp["Time [Datetime]"] = (tmp["Time [Datetime]"]-tmp["Time [Datetime]"].iloc[0]).apply(lambda x: x.total_seconds()/60.0).values
-    tmp.rename(columns={"Time [Datetime]": "Time [Minutes]"}, inplace=True)
-    xfrm.solution = tmp
-    #Load loading conditions file
-    lc = LoadConditions()
-    lc.import_data(load_config)
-    print("complete.")
+    with pd.HDFStore(h5path, mode="r") as hdf:
+        xfrm = Transformer()
+        ## load transformer configuration
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as fp:
+            xfrmconfig = pd.read_hdf(hdf, "/xfrmconfig").loc["xfrm"].to_dict()
+            json.dump(xfrmconfig, fp)
+            fp.close()
+            xfrm.from_json(fp.name)
+            os.remove(fp.name)
+        
+        tmp = pd.read_hdf(hdf, key=key+"/solution")
+        # else: # csv
+        #     tmp = pd.read_csv(xfrmsol, parse_dates=[0])
+        tmp["Time [Datetime]"] = (tmp["Time [Datetime]"]-tmp["Time [Datetime]"].iloc[0]).apply(lambda x: x.total_seconds()/60.0).values
+        tmp.rename(columns={"Time [Datetime]": "Time [Minutes]"}, inplace=True)
+        xfrm.solution = tmp
+        
+        #Load loading conditions file
+        lc = LoadConditions()
+        load_config = pd.read_hdf(hdf, key=key+"/lc").to_csv(index=False)
+        lc.import_data(io.StringIO(load_config))
+        print("complete.")
 
     
     print("plotting...", end="")
@@ -25,7 +37,7 @@ def main(xfrm_config, load_config, savename):
         warnings.simplefilter("ignore", FutureWarning)
         fig, tab = xmdl.plotting.plot_results_plotly(xfrm,lc)
     # from https://stackoverflow.com/questions/59868987/saving-multiple-plots-into-a-single-html
-    with open(os.path.splitext(savename)[0]+".html","w") as f:
+    with open(savename,"w") as f:
         f.write(fig.to_html(include_plotlyjs="cdn"))
         f.write(tab.to_html(full_html=False, include_plotlyjs=False))
     print("complete.")
@@ -33,9 +45,9 @@ def main(xfrm_config, load_config, savename):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="transformer thermal modeling simulation")
-    parser.add_argument("xfrmconfig", help="transformer thermal namplate configuration json")
-    parser.add_argument("loadconfig", help="Load condition csv data")
-    parser.add_argument("savename", help="path with solution to plot")
+    parser.add_argument("h5path", help="h5 solution")
+    parser.add_argument("key", help="configuration key")
+    parser.add_argument("savename", help="save name for figure (html)")
     args = parser.parse_args()
     
-    main(args.xfrmconfig, args.loadconfig, args.savename)
+    main(args.h5path, args.key, args.savename)
--- ieee-c57.91-edit/scripts/run_simulation.py
+++ ieee-c57.91-edit/scripts/run_simulation.py
@@ -1,12 +1,67 @@
 
+import pandas as pd
 import transformer_thermal_models as xmdl
 from transformer_thermal_models import Transformer, LoadConditions
-import argparse
+import argparse, sys, os, io
+import tomli, json
+import itertools
 
-def main(xfrm_config, load_config, savename,
+def load_config(filepath, default_path=None):
+    
+    if default_path is not None:
+        with open(default_path, mode='rb') as f:
+            inputs = tomli.load(f)
+    else:
+        inputs = {}
+    
+    if filepath:
+        with open(filepath, mode='rb') as f:
+            user = tomli.load(f)
+    else:
+        user = {}
+
+    for k, v in user.items():
+        inputs[k] = v
+    
+    return inputs
+
+def print_inputs(args, logger=None):
+    """ print command line inputs and data type"""
+    if logger is not None:
+        print = logger.info
+    else:
+        import builtins
+        print = builtins.print
+    print("Inputs:\n==============")
+    if type(args) != dict:
+        for k, v in vars(args).items():
+            print(f'{k} : {v} type: {type(v)}') 
+    else:
+        for k, v in args.items():
+            print(f'{k} : {v} type: {type(v)}') 
+
+def mk_data(event_time, pnom:float, pover:float, 
+            t_amb:float=30):
+    
+    start_time = "2024-01-02 12:00"
+    out = {"Time": [pd.to_datetime(start_time) - pd.Timedelta(hours=12), 
+                    pd.to_datetime(start_time) - pd.Timedelta(minutes=5), 
+                    pd.to_datetime(start_time),
+                    pd.to_datetime(start_time) + pd.Timedelta(seconds=event_time),
+                    pd.to_datetime(start_time) + pd.Timedelta(hours=6),
+                    pd.to_datetime(start_time) + pd.Timedelta(hours=12)],
+           "Ambient": [t_amb]*6,
+           "Load":[pnom, pnom, pover, pnom, pnom, pnom],
+           "Plot": [False, True, True, True, False, False]
+    }
+
+    return pd.DataFrame(out)
+
+def main(xfrm_config, load_config, ret=True,
+         savename="",
          method="main_clause_7_diff"):
     
-    print("loading data...", end="")
+    print("\tloading data...", end="")
     #Load transformer data
     xfrm = Transformer()
     xfrm.from_json(xfrm_config)
@@ -17,23 +72,60 @@ def main(xfrm_config, load_config, savename,
     print("complete.")
 
     ## solve temperatures
-    print("solving model...", end="")
+    print("\tsolving model...", end="")
     xmdl.solve_temperatures(xfrm, lc, method)
     xmdl.solve_estimated_loss_of_life(xfrm, lc)
     print("complete.")
 
-    print(f"saving data to {savename}...", end="")
+    print(f"\texporting data...", end="")
     output = xmdl.export_data(xfrm, lc, sec_res=True, output_pandas=True)
-    output.to_csv(savename, index=False)
+    ## fixed headers
+    # output.rename(columns=lambda x: x.replace(" ","_").replace("[","").replace("]","").replace(".","").lower(), inplace=True)
     print("complete.")
+    if savename:
+        output.to_csv(savename, index=False)
+    if ret:
+        return output
     
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="transformer thermal modeling simulation")
-    parser.add_argument("xfrmconfig", help="transformer thermal namplate configuration json")
-    parser.add_argument("loadconfig", help="Load condition csv data")
-    parser.add_argument("savename", help="path to save csv output")
+    parser.add_argument("config", help="configuration toml file")
+    # parser.add_argument("xfrmconfig", help="transformer thermal namplate configuration json")
+    # parser.add_argument("loadconfig", help="Load condition csv data")
+    # parser.add_argument("savename", help="path to save csv output")
     parser.add_argument("--method", choices=xmdl.AVAILABLE_METHODS.keys(), help="solution method", default="main_clause_7_diff")
     args = parser.parse_args()
     
+    config = load_config(args.config)
+    config["method"] = args.method
+    if not config["loadconfig"]:
+        for k in ["pnom", "pover", "event_time"]:
+            if not isinstance(config[k], list):
+                config[k] = [config[k]]
+    print_inputs(config)
+    if not config["loadconfig"]:
+        with pd.HDFStore(os.path.join(config["results_path"], "res.h5"), mode="w", complevel=1) as hdf:
+            ## store transformer configuration
+            with open(config["xfrmconfig"]) as f:
+                pd.DataFrame(json.load(f), index=["xfrm"]).to_hdf(hdf, key="/xfrmconfig")
+            for pnom, pover, event_time in itertools.product(config["pnom"], config["pover"], config["event_time"]):
+                print(f"pnom {pnom} | pover {pover} | event_time {event_time}")
+                filename = f"ln{round(pnom*100)}_lv{round(pover*100)}_tv{event_time}.csv"
+                config_results = os.path.join(config["results_path"], "load_configs")
+                lc = mk_data(event_time, pnom, pover)
+                
+                ### save configuration
+                if not os.path.exists(config_results):
+                    os.mkdir(config_results)
+                lc.to_csv(os.path.join(config_results, filename), index=False)
+                
+                sim_results = os.path.join(config["results_path"], "results")
+
+                res = main(config["xfrmconfig"], io.StringIO(lc.to_csv(index=False)), ret=True)
+                key = f"/ln{round(pnom*100)}/lv{round(pover*100)}/tv{event_time}"
+                res.to_hdf(hdf, key=key+"/solution")
+                lc.to_hdf(hdf, key=key+"/lc")
+
+    sys.exit(0)
     main(args.xfrmconfig, args.loadconfig, args.savename, args.method)
