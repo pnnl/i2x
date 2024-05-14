3894577343cde1f74f0a2cef1e74fc0cac1a8877
Eran Schweitzer <eran.schweitzer@pnnl.gov>
2024-05-14 01:43:53 -0700

    Sequence HCA test working
    The test runs for 10 hours and matches previously saved results.
    
    Note that given the way the random seed is created, the same bus
    should always be chosen in this test.

--- test/hca_ts.py
+++ test/hca_ts.py
@@ -3,16 +3,50 @@ This file contains tests for the hosting capacity time series functionality
 """
 
 from i2x.der_hca import hca
+import numpy as np
 
 if __name__ == "__main__":
     config = {"choice": "ieee9500",
               "res_pv_frac": 0,
+              "pvcurve": "pclear",
               "solnmode": "YEARLY",
+              "min_converged_vpu": 0.5, #if voltages below 0.5 treat as not converged
               "hca_method": "sequence",
-              "numsteps": 1,
+              "change_lines_init": ["redirect hca_ts_IEEE9500prep.dss"],
+              "loadcurve": "loadshape4",
+              "numsteps": 1, #only solve one snapshot at a time
               "start_time": [0,0],
-              "end_time": [5,0],
-              "stepsize": 3600,
+              "end_time": [10,0], #testing on just 5 iterations
+              "stepsize": 3600,  #1 hr
+              "reg_control": {
+                    "disable_all": True, # disable all control, will be done via shape
+                    "regulator_shape": { # regulator shape mapping
+                        "vreg2_a": "regulator_taps/hca9500node_Mon_vreg2_a_1wdg.csv",
+                        "vreg2_b": "regulator_taps/hca9500node_Mon_vreg2_b_1wdg.csv",
+                        "vreg2_c": "regulator_taps/hca9500node_Mon_vreg2_c_1wdg.csv",
+                        "vreg3_a": "regulator_taps/hca9500node_Mon_vreg3_a_1wdg.csv",
+                        "vreg3_b": "regulator_taps/hca9500node_Mon_vreg3_b_1wdg.csv",
+                        "vreg3_c": "regulator_taps/hca9500node_Mon_vreg3_c_1wdg.csv",
+                        "vreg4_a": "regulator_taps/hca9500node_Mon_vreg4_a_1wdg.csv",
+                        "vreg4_b": "regulator_taps/hca9500node_Mon_vreg4_b_1wdg.csv",
+                        "vreg4_c": "regulator_taps/hca9500node_Mon_vreg4_c_1wdg.csv",
+                        "feeder_reg1a": "regulator_taps/hca9500node_Mon_feeder_reg1a_1wdg.csv",
+                        "feeder_reg1b": "regulator_taps/hca9500node_Mon_feeder_reg1b_1wdg.csv",
+                        "feeder_reg1c": "regulator_taps/hca9500node_Mon_feeder_reg1c_1wdg.csv",
+                        "feeder_reg2a": "regulator_taps/hca9500node_Mon_feeder_reg2a_1wdg.csv",
+                        "feeder_reg2b": "regulator_taps/hca9500node_Mon_feeder_reg2b_1wdg.csv",
+                        "feeder_reg2c": "regulator_taps/hca9500node_Mon_feeder_reg2c_1wdg.csv",
+                        "feeder_reg3a": "regulator_taps/hca9500node_Mon_feeder_reg3a_1wdg.csv",
+                        "feeder_reg3b": "regulator_taps/hca9500node_Mon_feeder_reg3b_1wdg.csv",
+                        "feeder_reg3c": "regulator_taps/hca9500node_Mon_feeder_reg3c_1wdg.csv",
+                    }
+              },
+              "storage_control": {
+                  "storage_shape": { # map load objects representing storage to shape
+                      "bat1": "bat1",
+                      "bat2": "bat2"
+                  }
+              },
               "metrics": {
                     "include": {
                         "voltage": ["vmin", "vmax"],
@@ -28,34 +62,61 @@ if __name__ == "__main__":
               "monitors":
                 {"volt_monitor_method": "none"},# don't add any additional voltage monitors
             } 
+    config["hca_log"] = {"print_hca_iter": True}
     # config["hca_log"] = {"loglevel": "debug"}
+    # config["debug_output"] = True
+    # config["end_time"] = [1,0]
     h = hca.HCA(config, print_config=True)
     # h.runbase()
     h.logger.info("imported change lines:")
     h.logger.info(h.change_lines)
-
-    h.runbase()
+    
+    ### First Timestep ###
+    
+    ### run the base needed for 2 reasons:
+    ## 1: sets up base metrics for comparison
+    ## 2: deactivation of regulator control happens here (TODO: maybe change this)
+    h.runbase(skipadditions=False)
+        
+    ### Perform HCA 
+    # recalculate=True means no resource will actually be added --> important for multiple time periods
+    # unset_active_bus=False means active bus will be kept so can be used in subsequent periods
     h.hca_round("pv", recalculate=True, unset_active_bus=False)
     bus = h.active_bus
-    h.step_solvetime()
+    h.step_solvetime() # time step
+    ### main loop ###
     while not h.is_endtime():
+        ## base needs to be re-run for each time step
+        # skipadditions=True means all the deterministic changes that are saved in history will not be applied twice
         h.runbase(skipadditions=True)
-        h.hca_round("pv", bus=bus, recalculate=True)
+
+        ## seed the capacity with the last calculated HC
+        Sij = {k: v for k,v in h.get_hc("pv", bus, latest=True)[0].items() if k in ["kw", "kva"]}
+        if Sij["kw"] == 0:
+            Sij = None
+        
+        ## calculate HCA specifying bus location and initial capacity
+        h.hca_round("pv", bus=bus, Sij=Sij, recalculate=True,
+                    bnd_strategy=("add", 500))#, set_sij_to_hc=True)
         h.step_solvetime()
+
     h.unset_active_bus()
     ## show the resulting hca for bus
     h.logger.info(f"Sequence HCA for bus {bus}")
     h.logger.info(h.get_hc("pv", bus))
-    # for i in range(3):
-    #     h.rundss(solvetime=h.solvetime)
-    #     h.step_solvetime()
 
-    # h.runbase()
-    # h.hca_round("pv", recalculate=True) 
-
+    kw_res = np.array([5297.9062,5209.3060,4376.2749,4227.7636,
+                       5042.6609,5030.4652,5029.2370,5028.0092,
+                       5004.6859,4996.1330])
+    
+    err = np.linalg.norm(h.get_hc("pv", bus)["kw"].values - kw_res, np.inf)
+    if err < 5: # current kw tolerance is hard coded as 5 kw
+        h.logger.info(f"Success! (max error ({err:0.4f}) is within 5 kW)")
+    else:
+        h.logger.info(f"Failed! max error is {err:0.4f} > 5 kW")
+    
     ##TODO:
-    ## If it's not possible to include inverter controls on anything other than pysystems and storage
-    ## will need to add as pv system to allow for different inverter controls
-    ## that means that the application of the curves will need to be more selective 
-    ## than applying to all as is currently the case
-    ## Alternatively we just assume that all inverters behave the same (unity power factor vs volt var etc.)
+    ## hca_control_mode and hca_control_pf are intended to allow different
+    ## inverter behavior for the HCA resource compared to others.
+    ## A) is this really desireable?
+    ## B) if yes, this needs to be implemented (parameters are currently not used)
